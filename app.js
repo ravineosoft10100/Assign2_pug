21456368cfd9b97e3117224b486b3961911f1a5c
unknown <ravicom571@gmail.com>
2022-11-30 20:29:00 +0530

    second commit

--- app.js
+++ app.js
@@ -2,69 +2,99 @@
 const express = require('express');
 const app = express();
 //Setting port
-const PORT = 3030;
+const PORT = 1030;
 //requiring fs
 const fs = require('fs');
 
 //setting view engine
-app.set('view engine','pug');
-app.set('task','./views');
+app.set('view engine', 'pug');
+app.set('task', './views');
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/static",express.static("public"));
+app.use("/static", express.static("public"));
 
 //define routes
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("home");
 })
 
-app.get("/about",(req,res)=>{
+app.get("/about", (req, res) => {
     res.render("about", {
-        image : './static/about.jpg'
+        image: './static/about.jpg'
     });
 })
 
-app.get("/services",(req,res)=>{
-    res.render("services",{
+app.get("/services", (req, res) => {
+    res.render("services", {
         image: './static/service.jpg'
     });
 })
 
-app.get("/contact",(req,res)=>{
+app.get("/contact", (req, res) => {
     res.render("contact");
 })
 
-app.get("/gallery",(req,res)=>{
-    // let arr = ;
-    res.render("gallery",{
-        image:  ['https://picsum.photos/seed/picsum/200/300','https://picsum.photos/200/300?grayscale','https://picsum.photos/id/237/200/300','https://picsum.photos/200/300?grayscale']
+app.get("/details", (req, res) => {
+    if (!fs.existsSync('./public/contacts/info.txt'))
+        res.send("<h2>file is not available</h2>");
+    let array = fs.readFileSync('./public/contacts/info.txt').toString().split('\n');
+    const newArray = filerArray(array);
+    res.render("details", {
+        result: newArray
     });
 })
 
-app.post("/get_contact",(req,res)=>{
-    let {name,email,contact,city}=req.body;
+function filerArray(arr){
+    const allDetails = [];
+    arr.forEach(data => {
+        if(data === ''){}
+        else allDetails.push(data);
+    })
+    return allDetails;
+}
 
-    let file = "./public/contacts/"+email;
-    let data = ('Name: '+name+'\nEmail: '+email+'\nContact: '+contact+'\nCity: '+city);
-
-    if(!fs.existsSync(file)){
-        fs.mkdir(file, (err)=>{
-            if(err) throw err;
-            else{
-                fs.writeFile(file+'/information.txt',data,(err)=>{
-                    res.render("welcome");
-                    if(err) throw err;
-                });
-            }
-        });
-
-    }else{
-        res.render("welcome1");
-    }
+app.get("/gallery", (req, res) => {
+    // let arr = ;
+    res.render("gallery", {
+        image: ['https://picsum.photos/seed/picsum/200/300', 'https://picsum.photos/200/300?grayscale', 'https://picsum.photos/id/237/200/300', 'https://picsum.photos/200/300?grayscale']
+    });
 })
 
-app.listen(PORT,(err)=>{
-    if(err) throw err;
+const check = [];
+app.post("/get_contact", (req, res) => {
+    let { name, email, contact, city } = req.body;
+
+    if (name === '' || email === '' || contact === '' || city === ''){
+        return res.send('<h1> Fields are missing... </h1>');
+    }
+    else if (emailExist(check, email)){
+        res.render('welcome1');
+    }
+    else {
+        check.push(email);
+        let data = (name + ',' + email + ',' + contact + ',' + city);
+        fs.appendFile('./public/contacts/info.txt', data +'\n', err => {
+            if (err) throw err;
+            res.redirect('./details');
+            
+        })
+    }
+ 
+
+})
+
+function emailExist(check, data) {
+    for (let i of check) {
+        console.log(i + " === " + data);
+        if (i == data) {
+            return true;
+        }
+    }
+    return false;
+}
+
+app.listen(PORT, (err) => {
+    if (err) throw err;
     else console.log(`PORT ${PORT}`);
 })
